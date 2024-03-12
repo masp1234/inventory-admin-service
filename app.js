afecb52e24042d8cf89a55506afc828bb626a3f3
masp1234 <mart2c04@stud.kea.dk>
2024-03-12 17:48:56 +0100

    Trying to fix azure deployment

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get("/test", async (req, res) => {
     res.send({ data: createdProduct.dataValues })
 })
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => console.log('Server is listening on port', PORT));
